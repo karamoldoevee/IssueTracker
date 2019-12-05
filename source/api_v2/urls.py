49f52a301339d754da32cee4e9975e183f47150f
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-12-05 11:35:35 +0600

    Написаны точки входа для задач.

---
+++ source/api_v2/urls.py
@@ -0,0 +1,19 @@
+from django.urls import include, path
+
+from rest_framework import routers
+
+from api_v2 import views
+
+router = routers.DefaultRouter()
+
+router.register(r'issues', views.IssueViewSet)
+
+app_name = 'api_v2'
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+]
